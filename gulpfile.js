40d57bddf108cf74c282bfaf3e61d2f0d6eefc36
Aryle <arylo.open@gmail.com>
2015-09-06 04:07:40 +0800

    Update Gulp add command line `gulp web` `browser-sync`  replace `gulp-connect` use `gulp-load-plugins`

--- gulpfile.js
+++ gulpfile.js
@@ -2,56 +2,69 @@
 
 var fs = require('fs');
 
+var browserSync = require('browser-sync').create();
+
 var gulp = require('gulp');
-var connect = require('gulp-connect');
-var clean = require('gulp-clean');
-var notify = require('gulp-notify');
-var rename = require('gulp-rename');
-var concat = require('gulp-concat');
-var stylus = require('gulp-stylus');
-var autoprefixer = require('gulp-autoprefixer');
-var minifyCss = require('gulp-minify-css');
-var sourcemaps = require('gulp-sourcemaps');
+var $ = require('gulp-load-plugins')();
 
 var sourcePath = 'src/';
 var targetPath = 'dest/';
 
 gulp.task('default', ['build', 'lint'], function() {
-    return notify('Deploy Success');
+    return $.notify('Deploy Success');
 });
 
-gulp.task('web', ['build'], function() {
-    return connect.server({
-        'port': 8000
+gulp.task('web', ['build', 'live'], function() {
+    return browserSync.init({
+        'files': '**',
+        'open': false,
+        'server': {
+            'baseDir': './',
+            'directory': true
+        }
     });
 });
 
 gulp.task('clean', function() {
     return gulp.src(targetPath)
-        .pipe(clean());
+        .pipe($.clean());
 });
 
-gulp.task('build', ['clean'], function() {
+gulp.task('build', ['clean'], function () {
+    return gulp.start('build:style');
+});
+
+gulp.task('build:style', function() {
     // Simple Module Build
     var arrPath = fs.readdirSync(sourcePath);
     arrPath.forEach (function (item) {
         var path = sourcePath + item + '/';
         return gulp.src([path + 'reset.styl', path + '{!reset,*}.styl'])
-            .pipe(stylus())
-            .pipe(concat(item + '.css'))
+            .pipe($.stylus())
+            .pipe($.concat(item + '.css'))
             .pipe(gulp.dest(targetPath + 'assets/'));
     });
     // All Modules Build
     gulp.src([sourcePath + 'Core/{reset,!reset,*}.styl', sourcePath + '{!Core,*}/*.styl'])
-        .pipe(stylus())
-        .pipe(concat('lafite.css'))
-        .pipe(autoprefixer())
+        .pipe($.stylus())
+        .pipe($.concat('lafite.css'))
+        .pipe($.autoprefixer())
         .pipe(gulp.dest(targetPath))
-        .pipe(minifyCss())
-        .pipe(rename('lafite.min.css'))
+        .pipe($.minifyCss())
+        .pipe($.rename('lafite.min.css'))
         .pipe(gulp.dest(targetPath));
 });
 
-gulp.task('lint', ['build'], function() {
+gulp.task('live', ['build'], function () {
+    $.watch(sourcePath + '{**,}*.{css,styl}', function () {
+        gulp.start('build:style')
+            .pipe(browserSync.reload);
+    });
+    $.watch(sourcePath + '{**,}*.{htm,html}', function () {
+        gulp.pipe(browserSync.reload);
+    });
+});
+
+gulp.task('lint', ['build'], function () {
     // content
 });
